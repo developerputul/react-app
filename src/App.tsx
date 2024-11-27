77361333988f64f01d388220ef810758cef7c0d5
developerputul <putulsentongchangya7@gmail.com>
2024-11-27 14:50:48 +0600

    ListGroup Section Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
-import Button from "./components/button";
+import ListGroup from "./components/ListGroup/ListGroup";
+import './App.css';
 
-function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+function App(){
+  const items = ["Chittagong", "Bandarban", "Dhaka", "Khulna"];
 
-    return (
-      <div>
-        { alertVisible && <Alert onClose={() => setAlertVisibility(false)}> My Alert</Alert>}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button</Button>
-      </div>
-    );
+return (
+  <div>
+    <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
+  </div>
+);
+
 }
 
 export default App;
